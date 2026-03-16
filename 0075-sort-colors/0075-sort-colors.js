ce93d943c7eedd82fc94948e1258ad42131ae55f
AbdulHadhi-m <abdulhadhi.m007@gmail.com>
2026-03-16 16:04:04 +0530

    Time: 0 ms (100%), Space: 54.2 MB (29.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -2,6 +2,6 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
+var sortColors = function(nums) {            
     nums.sort((a, b) => a - b);
-};
+}; 
