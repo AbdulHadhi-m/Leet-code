c82c094bbf638b7333bd03c7a701318590ce9690
AbdulHadhi-m <abdulhadhi.m007@gmail.com>
2026-03-30 12:18:02 +0530

    Time: 5 ms (54.14%), Space: 60.6 MB (81.59%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,10 @@
+var romanToInt = function(s) {
+    const m = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+    let ans = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        ans += m[s[i]] < m[s[i + 1]] ? -m[s[i]] : m[s[i]];
+    }
+
+    return ans;
+};
