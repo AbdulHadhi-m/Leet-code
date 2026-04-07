263143eaeb16a2db65c01e8428960334f06fa8ec
AbdulHadhi-m <abdulhadhi.m007@gmail.com>
2026-04-07 23:56:33 +0530

    Time: 65 ms (25%), Space: 60.1 MB (37.5%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.js
@@ -0,0 +1,11 @@
+var maxDepth = function(root) {
+    if (root === null) return 0;
+
+    let depth = 0;
+
+    for (let child of root.children) {
+        depth = Math.max(depth, maxDepth(child));
+    }
+
+    return depth + 1;
+};
