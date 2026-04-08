3ca1f516c7ad997efdbb24cfd26279d636a0bee0
AbdulHadhi-m <abdulhadhi.m007@gmail.com>
2026-04-09 01:38:48 +0530

    Time: 0 ms (100%), Space: 55.9 MB (66.55%) - LeetHub

---
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.js
@@ -0,0 +1,8 @@
+var trimTrailingVowels = function(s) {
+  const vowels = new Set(["a", "e", "i", "o", "u"]);
+  let i = s.length - 1;
+
+  while (i >= 0 && vowels.has(s[i])) i--;
+
+  return s.slice(0, i + 1);
+};
