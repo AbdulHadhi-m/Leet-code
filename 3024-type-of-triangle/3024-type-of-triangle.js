a80b3b6bd7203ec62c7b5283c63089d3359e6321
Abdul Hadhi <abdulhadhi.m007@gmail.com>
2026-04-13 16:52:52 +0530

    Time: 1 ms (32.12%), Space: 55.7 MB (29.93%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.js
@@ -0,0 +1,17 @@
+var triangleType = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    if (nums[0] + nums[1] <= nums[2]) {
+        return "none";
+    }
+
+    if (nums[0] === nums[2]) {
+        return "equilateral";
+    }
+
+    if (nums[0] === nums[1] || nums[1] === nums[2]) {
+        return "isosceles";
+    }
+
+    return "scalene";
+};
