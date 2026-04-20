b6f2595303e8186446553bd2ba4561d34aa15c23
Abdul Hadhi <abdulhadhi.m007@gmail.com>
2026-04-20 15:36:52 +0530

    Time: 7 ms (79.67%), Space: 62.3 MB (41.06%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
@@ -0,0 +1,22 @@
+var findRestaurant = function(list1, list2) {
+    let map = new Map(), min = Infinity, res = [];
+
+    for (let i = 0; i < list1.length; i++) {
+        map.set(list1[i], i);
+    }
+
+    for (let j = 0; j < list2.length; j++) {
+        if (map.has(list2[j])) {
+            let sum = j + map.get(list2[j]);
+
+            if (sum < min) {
+                min = sum;
+                res = [list2[j]];
+            } else if (sum === min) {
+                res.push(list2[j]);
+            }
+        }
+    }
+
+    return res;
+};
