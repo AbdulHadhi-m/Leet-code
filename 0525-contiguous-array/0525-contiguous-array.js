c3eb582615352b30f6e1eebbb8f1fe21a0d25362
Abdul Hadhi <abdulhadhi.m007@gmail.com>
2026-04-25 16:14:56 +0530

    Time: 13 ms (65.5%), Space: 62.8 MB (57.5%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -0,0 +1,19 @@
+function findMaxLength(nums) {
+    let sum = 0;
+    let maxLen = 0;
+
+    const map = new Map();
+    map.set(0, -1);
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i] === 0 ? -1 : 1;
+
+        if (map.has(sum)) {
+            maxLen = Math.max(maxLen, i - map.get(sum));
+        } else {
+            map.set(sum, i);
+        }
+    }
+
+    return maxLen;
+}
