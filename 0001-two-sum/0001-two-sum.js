32681de9d2a86743d03d16be012352e0d1697bfa
Abdul Hadhi <abdulhadhi.m007@gmail.com>
2026-05-03 11:18:28 +0530

    Time: 1 ms (85.85%), Space: 56 MB (19.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,11 +1,7 @@
-var twoSum = function (nums,target){
-for(let i = 0; i < nums.length; i++){
-    for(let j = i + 1;j <nums.length;j++){
-        if(nums[i] + nums[j] === target){
-            return [i,j]
-        }
-    }
-}
-    
-}
-
+const twoSum = (nums, target) => {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[target - nums[i]] !== undefined) return [map[target - nums[i]], i];
+    map[nums[i]] = i;
+  }
+};
