7719ec63851590f48ada16ae0d71eb8a0106f416
Abdul Hadhi <abdulhadhi.m007@gmail.com>
2026-05-13 14:08:00 +0530

    Time: 0 ms (100%), Space: 52.5 MB (95.67%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -1,20 +1,18 @@
-/**
- * @param {number[]} digits
- * @return {number[]}
- */
-
-var plusOne = function(digits) {
-
+function plusOne(digits) {
+    // Start from the last digit
     for (let i = digits.length - 1; i >= 0; i--) {
-
+        // If digit is less than 9, just add 1
         if (digits[i] < 9) {
-            digits[i]++;        
-            return digits;      
+            digits[i]++;
+            return digits;
         }
 
-        digits[i] = 0;         
+        // If digit is 9, make it 0
+        digits[i] = 0;
     }
 
-    digits.unshift(1);         
+    // If all digits were 9
+    digits.unshift(1);
+
     return digits;
-};
+}
